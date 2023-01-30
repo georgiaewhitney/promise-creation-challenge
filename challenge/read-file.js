d474416482d0cb5b3fe93244cef009fdaa865495
Georgia E. Whitney <81175151+rockyrelay@users.noreply.github.com>
2023-01-30 12:52:38 +0000

    additional explanatory notes

--- challenge/read-file.js
+++ challenge/read-file.js
@@ -1,7 +1,12 @@
 const fs = require("node:fs");
 
+// function defined
 function readFilePromise(filePath) {
-  //
+  // returns promise that:
+  // returns contents of a file using fs.readFile method from built in fs node module
+  // if successful, file contents passed to resolve function indicating fulfillment
+  //if there's an error, error passed to reject func indicating rejection
+  // returned promise can be used to perform async fil;e reading in a way that can be easily composed with other async operation
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, (error, contents) => {
       if (error) {
@@ -14,5 +19,3 @@ function readFilePromise(filePath) {
 }
 
 module.exports = { readFilePromise };
-
-// The code defines a function readFilePromise which takes a file path as an argument and returns a Promise that:  Returns the contents of a file using the fs.readFile method from the built-in fs module in Node.js. If the fs.readFile operation is successful, the contents of the file are passed to the resolve function to indicate that the Promise is fulfilled. If there is an error reading the file, the error is passed to the reject function to indicate that the Promise is rejected. The Promise returned by readFilePromise can be used to perform asynchronous file reading in a way that can be easily composed with other asynchronous operations.
