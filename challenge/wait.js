fcd328aff13096e150817a4d0672b1023632700e
Georgia E. Whitney <81175151+rockyrelay@users.noreply.github.com>
2023-01-30 12:28:56 +0000

    test 1 passed

--- challenge/wait.js
+++ challenge/wait.js
@@ -1,5 +1,19 @@
 function wait(ms) {
   //
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("done");
+  }, ms);
+  });
+  return promise;
 }
 
+wait(1000).then(() => console.log("done"));
+wait(300).then(() => console.log("done after 300ms"));
+
+wait()
+
 module.exports = { wait };
+
+
